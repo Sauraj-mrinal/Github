c7fbf9643361f182a420a0a997182d6f9f7243d5
Sauraj-mrinal <msauraji@gmail.com>
2023-11-06 12:54:57 +0530

    Add

---
+++ sharpeper/Booking Appointment App/functionality of edit Appointments/index.js
@@ -0,0 +1,6 @@
+// The user should be able to edit the user detail of an existing user by clicking on the Edit icon placed next to the user.
+
+// So place a small edit icon next to user detail
+// On clicking the edit icon, automatically populate the main registration form with the userdetails so that user can edit his details.
+// On Submiting this form it should it should edit the user details of the same user.
+// Once we click on submit , the website should automatically get updated with new user details
